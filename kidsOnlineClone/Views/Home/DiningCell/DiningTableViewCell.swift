ece219d61bebc81542dea6662bb68db755219034
Hua Son Tung <tunghuason@gmail.com>
2021-09-27 17:23:12 +0700

    Create mealIcon, verticalLine, mealLabel, mealTime, mealDetail and connect to nib

--- kidsOnlineClone/Views/Home/DiningCell/DiningTableViewCell.swift
+++ kidsOnlineClone/Views/Home/DiningCell/DiningTableViewCell.swift
@@ -11,6 +11,12 @@ class DiningTableViewCell: UITableViewCell {
 
     static let identifier = "diningCell"
     
+    @IBOutlet var mealIcon: UIImageView!
+    @IBOutlet var verticalLine: UIView!
+    @IBOutlet var mealLabel: UILabel!
+    @IBOutlet var mealTime: UILabel!
+    @IBOutlet var mealDetail: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
