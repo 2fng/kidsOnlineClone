aeaaa0e104cdf59946d718724c3a0130b8c88bd2
Hua Son Tung <tunghuason@gmail.com>
2021-10-02 11:37:54 +0700

    create identifier, nib function, setup outlet

--- kidsOnlineClone/Views/Home/DiningCell/DiningFooterView.swift
+++ kidsOnlineClone/Views/Home/DiningCell/DiningFooterView.swift
@@ -8,6 +8,16 @@
 import UIKit
 
 class DiningFooterView: UIView {
+    
+    static let identifier = "footerViewCell"
 
-
+    @IBOutlet weak var cellContentView: UIView!
+    @IBOutlet weak var cellTextField: UITextField!
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var cellButton: UIButton!
+    
+    static func nib() -> UINib {
+        return UINib(nibName: "DiningFooterView", bundle: nil)
+    }
+    
 }
