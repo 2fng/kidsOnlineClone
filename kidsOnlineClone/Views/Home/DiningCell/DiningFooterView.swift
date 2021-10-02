79ff08c31857aedb3609aded8e88341a14723e9b
Hua Son Tung <tunghuason@gmail.com>
2021-10-02 11:33:37 +0700

    Create footerView for DiningView

---
+++ kidsOnlineClone/Views/Home/DiningCell/DiningFooterView.swift
@@ -0,0 +1,20 @@
+//
+//  DiningFooterView.swift
+//  kidsOnlineClone
+//
+//  Created by Hua Son Tung on 02/10/2021.
+//
+
+import UIKit
+
+class DiningFooterView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
